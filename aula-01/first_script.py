787466c3586236a6fe2f37ee7f79719cf373c443
Paulo Oliveira <paulo.oliveira@avios.com>
2024-08-20 22:45:48 +0100

    Primeiro script usando python e selenium

---
+++ aula-01/first_script.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from time import sleep
+
+browser = webdriver.Firefox()
+browser.get("https://www.uol.com.br")
+
+browser.maximize_window()
+
+title = browser.title
+print(title)
+
+sleep(10)
+
+browser.quit()
